e949ed7450b9d99ee61764bb1ccd0739b3a7b0a3
cld <cdubois@octo.com>
2014-03-01 15:42:47 +0100

    original push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dogs::Application.config.secret_key_base = '5df3c5bf04561d91b6726485133b54429c453412bd83c51671a733632adb346f3affced16c1a2acb8b55bf67f2f1239ebc0e83c995ec733d22d8fe1b9ec11eb7'
